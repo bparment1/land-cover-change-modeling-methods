8528aa5cab24f8c23c462baa983d1ab6cf12652d
bparment1 <benoit.parmentier@gmail.com>
2018-05-09 14:30:00 -0400

    changes to run_ltm

--- lucc_case_study_sampling_strategy_functions.R
+++ lucc_case_study_sampling_strategy_functions.R
@@ -67,100 +67,127 @@ read_data <- function(matfile, xvr, yvr, IndLU_t1, IndLU_t2, CodeNU, CodeU,in_di
 }
 
 run_ltm <- function(change1, no_change1, xvr, m, yvr, ratio, K, sampling_name) { # IndLU_t1, IndLU_t2, CodeNU, CodeU
-# r$change, r$n_ch
-
-dataset1 = rbind( cbind(change1, rep(1, nrow(change1)) ), cbind(no_change1, rep(0, nrow(no_change1) ))) ### 
-
-dataset1N <- fn.normalize(dataset1, xvr, yvr) # dataset1 
-
-	# data split  according to the sampling strategy 
-
-# LT  = eval(as.call(list(as.name(sampling_name), dataset1N, yvr, ratio))) 
-# FUN <- match.fun(sampling_name) 
-
-LT = sampling_name(dataset1N, yvr, ratio)
-T = LT$T 
-L = LT$T
- 
-########################################
-# LTM, 		without clustering prior learning 
-########################################
-# install.packages('TOC') # library(TOC)
-
-##############################################################################################################################
-###############################################################
-# run the LTM model, without clustering prior learning 
-###############################################################
-##############################################################################################################################
-
-#############
-threshold = round(nrow(T[T[,yvr]==1, ]) / nrow(T), 2)  # T ; dataset1N
-print("th = ") 
-print(threshold) 
-#############
-
-newT = fnLUCAnalysis_maxP(L, T, xvr, yvr, m, threshold) # dataset1N; dataset1N, T, rbind(L,T)
-###
-
-cm = table(newT[,yvr], newT[,yvr+2]) 
-print("cross-tab from the LTM model: ")
-print(cm)
-
-errors = apply(newT, 1, metrics, yvr, yvr+2)
-
-# Total Operating Characteristic - TOC and ROC curves
-
-tocd <- TOC(newT[,yvr], newT[,yvr+2], mask=NULL, nthres = 100)
-
-pcm = pcm_evaluation(cm)
-# mean(pcm)
-
-res= list(pcm_ltm = pcm, 
-cm_ltm = cm, 
-errors_ltm = errors, 
-toc_ltm =tocd,  
-change_ltm = change1, no_change_ltm = no_change1, newT = newT)
-
-return(res)
+  # 
+  # This functions runs a neural network model with binary input data.
+  # INPUTS:
+  # 1) change1
+  # 2) no_change2
+  # 3) xvr: index range for the covariates used in the model
+  # 4) m: number of covariates
+  # 5) yvr: index for the y variable
+  
+  ################# Start script ####################
+  
+  #########################
+  #### Part 1: generate training and testing and run LTM
+  
+  ### Careful sampling name is a function!!!!!
+  
+  dataset1 = rbind( cbind(change1, rep(1, nrow(change1)) ), cbind(no_change1, rep(0, nrow(no_change1) ))) ### 
+  
+  ### This is a user defined function, rescaling
+  dataset1N <- fn.normalize(dataset1, xvr, yvr) # dataset1, scaling by min and max: x- min(x)/(max (x) -min(x))
+  # data split  according to the sampling strategy 
+  
+  # LT  = eval(as.call(list(as.name(sampling_name), dataset1N, yvr, ratio))) 
+  # FUN <- match.fun(sampling_name) 
+  
+  #### ?
+  #debug(sampling_name)!!! This is a function
+  LT = sampling_name(dataset1N, yvr, ratio) # output is alist
+  T = LT$T # matrix of data for testing
+  L = LT$T # matrix of data for training
+  
+  ########################################
+  # LTM, 		without clustering prior learning 
+  ########################################
+  # install.packages('TOC') # library(TOC)
+  
+  ##############################################################################################################################
+  ###############################################################
+  # run the LTM model, without clustering prior learning 
+  ###############################################################
+  ##############################################################################################################################
+  
+  #what is the use of the variable threshold?
+  #############
+  threshold = round(nrow(T[T[,yvr]==1, ]) / nrow(T), 2)  # T ; dataset1N
+  print("th = ") 
+  print(threshold) 
+  #############
+  
+  #### THis function run LTM (nnet) for training and testing
+  newT = fnLUCAnalysis_maxP(L, T, xvr, yvr, m, threshold) # dataset1N; dataset1N, T, rbind(L,T)
+  #newT is a matrix with same size but two more columns, one for the activation and one for the hardening into one and zero
+  ###
+  
+  ###########################
+  ###### Part 2: generate accuracy metrics #####
+  
+  ### Crosstabutlation of observed versus predicted
+  cm = table(newT[,yvr], newT[,yvr+2]) 
+  
+  print("cross-tab from the LTM model: ")
+  print(cm)
+  
+  errors = apply(newT, 1, metrics, yvr, yvr+2)
+  
+  # Total Operating Characteristic - TOC and ROC curves
+  
+  tocd <- TOC(newT[,yvr], newT[,yvr+2], mask=NULL, nthres = 100)
+  
+  pcm = pcm_evaluation(cm)
+  # mean(pcm)
+  
+  
+  
+  ##### This is the return object with all the results
+  res= list(pcm_ltm = pcm, 
+            cm_ltm = cm, 
+            errors_ltm = errors, 
+            toc_ltm =tocd,  
+            change_ltm = change1, no_change_ltm = no_change1, newT = newT)
+  
+  return(res)
 }
 
 
 fct_result <-  function (res_case, IT) { # IT is the number of replications 
-
-auc_ltm = c()
-pcm_ltm = c()
-ER_ltm = c()
-
-for (i in 1:IT) {
-
-auc_ltm = c( auc_ltm, res_case[,i]$toc_ltm@AUC )
-
-pcm_ltm = c( pcm_ltm, mean(res_case[,i]$pcm_ltm))
-
-ER_ltm = c(ER_ltm, round((res_case[,i]$cm_ltm[1,2] + res_case[,i]$cm_ltm[2,1]) * 100 / sum(res_case[,i]$cm_ltm),3) )
-
-}
-
-print(auc_ltm)
-
-print(ER_ltm)
-
-print(pcm_ltm)
-
-auc_ltmMSD = list(mean = mean(auc_ltm) , sd = sd(auc_ltm))
-pcm_ltmMSD = list(mean = mean(pcm_ltm), sd = sd(pcm_ltm))
-ER_ltmMSD = list(mean = mean(ER_ltm), sd = sd(ER_ltm))
-
-result_mean = c(AUC = auc_ltmMSD$mean, PCM = pcm_ltmMSD$mean, ER = ER_ltmMSD$mean) 
-
-result_sd =   c(AUC = auc_ltmMSD$sd, PCM = pcm_ltmMSD$sd, ER = ER_ltmMSD$sd) 
-
-# row.names(result_mean) <- c("res_ltm_mean")
-# row.names(result_sd) <- c("res_ltm_sd")
-
-result = list(mean = result_mean, sd = result_sd)
-
-return(result)
+  
+  auc_ltm = c()
+  pcm_ltm = c()
+  ER_ltm = c()
+  
+  for (i in 1:IT) {
+    
+    auc_ltm = c( auc_ltm, res_case[,i]$toc_ltm@AUC )
+    
+    pcm_ltm = c( pcm_ltm, mean(res_case[,i]$pcm_ltm))
+    
+    ER_ltm = c(ER_ltm, round((res_case[,i]$cm_ltm[1,2] + res_case[,i]$cm_ltm[2,1]) * 100 / sum(res_case[,i]$cm_ltm),3) )
+    
+  }
+  
+  print(auc_ltm)
+  
+  print(ER_ltm)
+  
+  print(pcm_ltm)
+  
+  auc_ltmMSD = list(mean = mean(auc_ltm) , sd = sd(auc_ltm))
+  pcm_ltmMSD = list(mean = mean(pcm_ltm), sd = sd(pcm_ltm))
+  ER_ltmMSD = list(mean = mean(ER_ltm), sd = sd(ER_ltm))
+  
+  result_mean = c(AUC = auc_ltmMSD$mean, PCM = pcm_ltmMSD$mean, ER = ER_ltmMSD$mean) 
+  
+  result_sd =   c(AUC = auc_ltmMSD$sd, PCM = pcm_ltmMSD$sd, ER = ER_ltmMSD$sd) 
+  
+  # row.names(result_mean) <- c("res_ltm_mean")
+  # row.names(result_sd) <- c("res_ltm_sd")
+  
+  result = list(mean = result_mean, sd = result_sd)
+  
+  return(result)
 }
 
 
